fab82bf8e083ae2d8467ec6c2c711def203ea5e4
gabicilfone <cilfonegabriel@gmail.com>
2023-10-03 17:40:43 -0300

    Add meta information

--- app/root.jsx
+++ app/root.jsx
@@ -1,20 +1,28 @@
-export default function App() {
-    return(
-        <Document>
-            <h1>Hola mundo</h1>
-        </Document>
-    )
+import { Meta } from '@remix-run/react';
 
-    function Document({children}) {
-        return (
-            <html lang="es">
-                <head>
-                    <title>GuitarLA - Remix</title>
-                </head>
-                <body>
-                    {children}
-                </body>
-            </html>
-        )
-    }
+export function meta() {
+  return [
+    { charset:'utf-8' },
+    { title:'GuitarLA - Remix' },
+    { viewport:'width=device-width, initial-scale=1' }
+  ];
+}
+
+export default function App() {
+  return (
+    <Document>
+      <h1>Hola mundo</h1>
+    </Document>
+  );
+}
+
+function Document({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <Meta />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
