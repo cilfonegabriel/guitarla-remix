fa58a4f07cd454adab53bdbb7bb7a662d831bf98
gabicilfone <cilfonegabriel@gmail.com>
2023-10-03 18:00:22 -0300

    add external style sheets

--- app/root.jsx
+++ app/root.jsx
@@ -9,13 +9,30 @@ export function meta() {
   ];
 }
 
+
 export function links() {
     return[
+        {
+            rel: 'stylesheet',
+            href : 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+        },
+        {
+            rel:"preconnect",
+            href:"https://fonts.googleapis.com"
+        },
+        {
+            rel:"preconnect",
+            href:"https://fonts.gstatic.com",
+            crossorigin: "true"
+        },
+        {
+            rel:"stylesheet",
+            href:"https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        },
         {
             rel: 'StyleSheet',
             href: styles
         }
-
     ]
 }
 
