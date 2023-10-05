e143e3d10214968fbd3447bbec5f7d25da0fad96
gabicilfone <cilfonegabriel@gmail.com>
2023-10-05 08:06:48 -0300

    Add style to the us page

--- app/routes/us.jsx
+++ app/routes/us.jsx
@@ -1,4 +1,19 @@
 import imagen from '../../public/img/nosotros.jpg'
+import styles from '../styles/us.css'
+
+export function links() {
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image'
+    }
+  ]
+}
 
 function Us() {
   return (
@@ -13,7 +28,7 @@ function Us() {
             Vestibulum eget neque massa. Praesent lacinia risus elit, sit amet lobortis risus iaculis sit amet. Curabitur bibendum neque quis nunc egestas, vel eleifend dui efficitur. 
             Pellentesque porttitor ante urna, ac hendrerit nisi sodales nec. Donec mattis dapibus nulla at congue. Integer eu tortor volutpat, tempor mi at, congue risus. Vivamus sed quam sed metus interdum tempor sed nec diam.
           </p>
-          
+
           <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
             Sed ullamcorper ipsum quis posuere dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
             Vestibulum eget neque massa. Praesent lacinia risus elit, sit amet lobortis risus iaculis sit amet. Curabitur bibendum neque quis nunc egestas, vel eleifend dui efficitur. 
