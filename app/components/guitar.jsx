aa2da9d1043ec1ca9d384921ef189bb4f4707c40
gabicilfone <cilfonegabriel@gmail.com>
2023-10-05 17:08:16 -0300

    Show description of the guitars

--- app/components/guitar.jsx
+++ app/components/guitar.jsx
@@ -1,10 +1,22 @@
+import { Link } from "@remix-run/react"
 
-function Guitar({guitar}) {
+export function Guitar({guitar}) {
 
-    console.log(guitar)
+    const { description, imagen, price, url, name } = guitar
+
+
   return (
-    <div>
-        Guitar
+    <div className="guitarra">
+        <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitar &{name}`} />
+        <div className="contenido">
+            <h3>{name}</h3>
+            <p className="descripcion">{description}</p>
+            <p className="precio">${price}</p>
+
+            <Link className="enlace" to={`/guitars/${url}`}>
+                See Products
+            </Link>
+        </div>
     </div>
   )
 }
