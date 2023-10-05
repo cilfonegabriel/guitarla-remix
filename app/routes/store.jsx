2c248ca4f0f1b7a63e0fb1ef052cf2d625f29a62
gabicilfone <cilfonegabriel@gmail.com>
2023-10-05 15:06:02 -0300

    Consuming the strapi backend with loader

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,4 +1,14 @@
 
+
+export async function loader(){
+  const resp = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const result = await resp.json()
+
+  return {
+
+}
+}
+
 function Store() {
   return (
     <div>
