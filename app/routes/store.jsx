5f7e17c8a41e15625770d061752cda0f53594386
gabicilfone <cilfonegabriel@gmail.com>
2023-10-05 17:35:36 -0300

    Add css to guitar store

--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,6 +1,24 @@
 import { getGuitars } from '../models/guitars.server'
 import { useLoaderData } from '@remix-run/react'
 import Guitar from '../components/guitar'
+import styles from '../styles/guitars.css'
+
+export function meta() {
+  return [
+    { title:'GuitarLA - Guitar Store' },
+    { description:'GuitarLA - Our guitar collection' }
+  ];
+}
+
+
+export function links() {
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader(){
   const guitars = await getGuitars()
