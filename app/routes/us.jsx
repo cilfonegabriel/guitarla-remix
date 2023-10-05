461df3ce998949e7fd2f32ff3d565cc7758f5bcd
gabicilfone <cilfonegabriel@gmail.com>
2023-10-05 07:44:34 -0300

    Create page Us

--- app/routes/us.jsx
+++ app/routes/us.jsx
@@ -1,9 +1,27 @@
+import imagen from '../../public/img/nosotros.jpg'
 
 function Us() {
   return (
-    <div>
-      Desde Us
-    </div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Us</h2>
+
+      <div className="contenido">
+        <img src={imagen} alt="Imagen about us" />
+        <div>
+          <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+            Sed ullamcorper ipsum quis posuere dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+            Vestibulum eget neque massa. Praesent lacinia risus elit, sit amet lobortis risus iaculis sit amet. Curabitur bibendum neque quis nunc egestas, vel eleifend dui efficitur. 
+            Pellentesque porttitor ante urna, ac hendrerit nisi sodales nec. Donec mattis dapibus nulla at congue. Integer eu tortor volutpat, tempor mi at, congue risus. Vivamus sed quam sed metus interdum tempor sed nec diam.
+          </p>
+          
+          <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+            Sed ullamcorper ipsum quis posuere dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+            Vestibulum eget neque massa. Praesent lacinia risus elit, sit amet lobortis risus iaculis sit amet. Curabitur bibendum neque quis nunc egestas, vel eleifend dui efficitur. 
+            Pellentesque porttitor ante urna, ac hendrerit nisi sodales nec. Donec mattis dapibus nulla at congue. Integer eu tortor volutpat, tempor mi at, congue risus. Vivamus sed quam sed metus interdum tempor sed nec diam.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
