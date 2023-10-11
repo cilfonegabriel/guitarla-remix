c0588ee6fcfbcaba5084b906236c0aa8edaf82b7
gabicilfone <cilfonegabriel@gmail.com>
2023-10-11 20:50:51 -0300

    Add style to Shopping Cart

--- app/routes/shopping.jsx
+++ app/routes/shopping.jsx
@@ -1,9 +1,38 @@
+import styles from '../styles/shopping.css'
+
+
+export function links() {
+    return[
+      {
+        rel: 'stylesheet',
+        href: styles
+      }
+    ]
+}
+
+export function meta() {
+    return [
+      { title:'GuitarLA - Shopping Cart' },
+      { description:'Sale of guitars, music, blog, shopping cart, store' }
+    ];
+}
 
 function Shopping() {
   return (
-    <div>
-      carrito
-    </div>
+    <main className="contenedor">
+        <h1 className="heading">Shopping Cart</h1>
+        <div className="contenido"> 
+            <div className='carrito'>
+                <h2>Articles</h2>
+            </div>
+            <aside className="resumen">
+                <h3>Order Summary</h3>
+                <p>Total to pay: $</p>
+            </aside>
+        </div>
+
+
+    </main>
   )
 }
 
